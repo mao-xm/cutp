f57483d5cda0abebbc47e82d1c275c9b82457e71
mengying <2237914905@qq.com>
2020-03-25 15:23:08 +0800

    qq

--- src/views/buyOrder/Index.vue
+++ src/views/buyOrder/Index.vue
@@ -1,16 +1,11 @@
 <template>
   <div class="buyOrder">
-    <div style="margin-top: 15px;">
-      <el-input placeholder="请输入内容" v-model="input3" class="input-with-select">
-        <el-select v-model="select" slot="prepend" placeholder="请选择">
-          <el-option label="餐厅名" value="1"></el-option>
-          <el-option label="订单号" value="2"></el-option>
-          <el-option label="用户电话" value="3"></el-option>
-        </el-select>
-        <el-button slot="append" icon="el-icon-search"></el-button>
-      </el-input>
-    </div>
     <el-card class="buyOrder-card">
+      <div class="buyOrder-search">
+        <el-input placeholder="请输入内容" v-model="input3" class="buyOrder-search-input">
+          <el-button slot="append" icon="el-icon-search"></el-button>
+        </el-input>
+      </div>
       <el-tabs v-model="activeName" @tab-click="handleClick" active-text-color="red">
         <el-tab-pane label="全部" name="all">全部</el-tab-pane>
         <el-tab-pane label="待付款" name="waitPay">待付款</el-tab-pane>
@@ -41,6 +36,16 @@ export default {
   position: relative;
   margin: auto auto;
   width: 80%;
+}
+.buyOrder-search{
+  position: relative;
+  height: 40px;
+  width: 200px;
+  float: right;
+  z-index: 1;
+}
+.buyOrder-search-input{
+  height: 30px !important;
 }
 .el-tabs__active-bar{
   background-color: red !important;

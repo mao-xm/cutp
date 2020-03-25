ced1e798b00cbc43bf50b429d661678e4c430325
lxxs226 <1203821778@qq.com>
2020-03-25 15:48:05 +0800

    拉

--- src/views/good/Home.vue
+++ src/views/good/Home.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="home">
     
-      <el-carousel :interval="5000" arrow="" height="350px">
-        <el-carousel-item v-for="item in 4" :key="item">
-        <h3>{{ item }}</h3>
+      <el-carousel :interval="5000" arrow="" height="400px" width="1200px">
+        <el-carousel-item v-for="item in picList" :key="item" >
+           <img :src="item.idView" class="home-pic">
         </el-carousel-item>
       </el-carousel>
     <!-- <router-link :to="{path:'/in_index'}">积分商城</router-link> -->
@@ -13,16 +13,31 @@
 </template>
 <script>
 export default {
-  name: 'Home'
-  
+  name: 'Home',
+   data () {
+      return {
+        picList: [
+          {id:0,idView:require('../../assets/goods/house.jpg')},
+          {id:1,idView:require('../../assets/goods/kouhon.png')},
+          {id:2,idView:require('../../assets/goods/dressnew.jpg')}
+        ]
+        }
+   }
 }
 </script>
 <style scoped>
 
 #home{
-  height: 450px;
-  width: 100%;
+  height: 500px;
+  width: 1200px;
   margin-top: 10px;
+   position: relative;
+}
+.home-pic{
+    /* max-width: 100%;
+    max-height: 100%; */
+    height: 100%;
+    width: 100%;
 }
  .el-carousel__item h3 {
     color: #475669;
